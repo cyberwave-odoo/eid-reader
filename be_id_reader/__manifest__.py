b71f87bef02bebb5a51cc2029399b7ee75586fd0
jer.dewandre@gmail.com <jer.dewandre@gmail.com>
2024-08-16 12:18:20 +0200

    Initial commit 17.0

--- be_id_reader/__manifest__.py
+++ be_id_reader/__manifest__.py
@@ -18,7 +18,7 @@
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '16.0.1.0.0',
+    'version': '17.0.1.0.0',
     
     'price': 99,
     'currency': 'EUR',
